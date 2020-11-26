d6e64e9d99f2ab87d7ec0399a39dea78968cb40d
DavidNovak1521 <novakdavid1521@gmail.com>
2020-11-26 20:33:47 +0100

    Printable

--- Item.hpp
+++ Item.hpp
@@ -2,15 +2,16 @@
 #define ITEM_HPP
 
 #include <string>
+#include "Printable.hpp"
 
-class Item
+class Item : Printable
 {
 public:
     Item(double weight) : weight(weight) {}
     virtual ~Item() {}
 
     double getWeight() const { return weight; }
-    virtual std::string toString() const { return "Item -> " + std::to_string(weight) + "kg\n"; }
+    virtual std::string toString() const override { return "Item -> " + std::to_string(weight) + "kg\n"; }
 
 private:
     const double weight;
